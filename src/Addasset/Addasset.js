fbaa064477bb1a7d5d1a29057c68815437aebda2
Tommy Trojan <rbelliss@usc.edu>
2021-03-13 22:16:00 -0800

    ryan update 2

--- src/Addasset/Addasset.js
+++ src/Addasset/Addasset.js
@@ -5,7 +5,10 @@ import AssetTableTab from '../AssetTableTab/AssetTableTab';
 import LineGraph from '../LineGraph/LineGraph';
 import GraphTab from '../GraphTab/GraphTab';
 import { Button } from '@material-ui/core';
-
+import{useState, useRef, setState} from 'react-hook-use-state';
+import Modal from 'react-modal';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 
 
@@ -15,34 +18,64 @@ export default class Addasset extends Component {
   	this.state = {
       portfolioValue:null,
   		stock:null,
-  		crypto:null
+  		crypto:null,
+      symbol:null,
+      shares:null
   	}
   }
 
-  add = () => {
-
- }
-
- remove = () => {
-
-}
 
   render(){
   	return (
       <div className="Addasset-wrapper">
       <AssetTableTab stock={this.state.stock} crypto={this.state.crypto} />
         <div className="addbutton-wrapper">
-            <Button variant="contained" color="primary" onClick={this.add}>
-            Add Asset
-          </Button>
+            <Popup trigger={<button > Add Asset </button>} position="right center">
+            <div><form>
+            <label>
+              Asset Symbol:
+              <input type="text" name="symbol" defaultValue={this.symbol} onChange={this.handleChange} />
+            </label>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <label>
+              Share Quantity:
+              <input type="text" name="0" defaultValue={this.shares} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          </div>
+            </Popup>
           &nbsp;
           &nbsp;
           &nbsp;
           &nbsp;
-          <Button variant="contained" color="primary" onClick={this.remove}>
-          Remove Asset
-        </Button>
+          <Popup trigger={<button > Remove Asset </button>} position="right center">
+          <div><form>
+          <label>
+            Asset Symbol:
+            <input type="text" name="symbol" defaultValue={this.symbol} onChange={this.handleChange}  />
+          </label>
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          <label>
+            Share Quantity:
+            <input type="text" name="0" defaultValue={this.shares} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         </div>
+          </Popup>
+        </div>
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+
       </div>
 
 	);
